60dcb8266b68c97fd38ab224d9febe4e0c7fb396
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-05-16 22:49:25 +0300

    Extending Collections

---
+++ Sixeyed.Extensions.Tests/ExtendingCollections/IReferenceDataSourceCollectionExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sixeyed.Extensions.ExtendingInterfaces;
+using Sixeyed.Extensions.ExtendingInterfaces.Implementation;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sixeyed.Extensions.Tests.ExtendingCollections
+{
+    [TestClass]
+    public class IReferenceDataSourceCollectionExtensionsTests
+    {
+        [TestMethod]
+        public void GetAllItemsByCode_Array()
+        {
+            var sources = new IReferenceDataSource[] { new SqlReferenceDataSource(),
+                new XmlReferenceDataSource(), new ApiReferenceDataSource()};
+            var items = sources.GetAllItemsByCode("xyz");
+            Assert.AreEqual(6, items.Count());
+        }
+
+        [TestMethod]
+        public void GetAllItemsByCode_ArrayList()
+        {
+            var sources = new ArrayList() { new SqlReferenceDataSource(),
+                new XmlReferenceDataSource(), new ApiReferenceDataSource()};
+            sources.Add("i am not a reference data source");
+            var items = sources.GetAllItemsByCode("xyz");
+            Assert.AreEqual(6, items.Count());
+        }
+
+        [TestMethod]
+        public void GetAllItemsByCode_IEnumerable()
+        {
+            var sources = new HashSet<IReferenceDataSource> { new SqlReferenceDataSource(),
+                new XmlReferenceDataSource(), new ApiReferenceDataSource()};
+            var items = sources.GetAllItemsByCode("xyz");
+            Assert.AreEqual(6, items.Count());
+        }
+    }
+}
